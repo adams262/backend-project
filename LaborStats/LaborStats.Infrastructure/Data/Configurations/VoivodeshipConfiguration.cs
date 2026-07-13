99f5d54c5c266aa0b9940c38834110478dbb7369
adams262 <adam.sobel11@gmail.com>
2026-07-13 19:21:53 +0200

    Add voivodeships and counties seed data

--- LaborStats/LaborStats.Infrastructure/Data/Configurations/VoivodeshipConfiguration.cs
+++ LaborStats/LaborStats.Infrastructure/Data/Configurations/VoivodeshipConfiguration.cs
@@ -18,5 +18,25 @@ public sealed class VoivodeshipConfiguration : IEntityTypeConfiguration<Voivodes
         builder.Property(v => v.Name)
             .HasMaxLength(64)
             .IsRequired();
+
+
+        builder.HasData(
+            new Voivodeship { Teryt = "02", Name = "DOLNOŚLĄSKIE" },
+            new Voivodeship { Teryt = "04", Name = "KUJAWSKO-POMORSKIE" },
+            new Voivodeship { Teryt = "06", Name = "LUBELSKIE" },
+            new Voivodeship { Teryt = "08", Name = "LUBUSKIE" },
+            new Voivodeship { Teryt = "10", Name = "ŁÓDZKIE" },
+            new Voivodeship { Teryt = "12", Name = "MAŁOPOLSKIE" },
+            new Voivodeship { Teryt = "14", Name = "MAZOWIECKIE" },
+            new Voivodeship { Teryt = "16", Name = "OPOLSKIE" },
+            new Voivodeship { Teryt = "18", Name = "PODKARPACKIE" },
+            new Voivodeship { Teryt = "20", Name = "PODLASKIE" },
+            new Voivodeship { Teryt = "22", Name = "POMORSKIE" },
+            new Voivodeship { Teryt = "24", Name = "ŚLĄSKIE" },
+            new Voivodeship { Teryt = "26", Name = "ŚWIĘTOKRZYSKIE" },
+            new Voivodeship { Teryt = "28", Name = "WARMIŃSKO-MAZURSKIE" },
+            new Voivodeship { Teryt = "30", Name = "WIELKOPOLSKIE" },
+            new Voivodeship { Teryt = "32", Name = "ZACHODNIOPOMORSKIE" }
+        );
     }
 }
