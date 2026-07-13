a145c54427ac8fb32ced53014711151024963c65
JJendrzej <Endriu7890@gmail.com>
2026-07-13 13:54:36 +0200

    Feature/users and roles entities setup (#8)
    * fix: add Professions and ProfessionGroups DbSets to DbContext
    
    * Add Users and Roles entities with config
    
    * Add migrations

---
+++ LaborStats/LaborStats.Domain/Entities/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace LaborStats.Domain.Entities;
+
+public sealed class Roles
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<Users> User { get; set; } = [];
+}
+
---
+++ LaborStats/LaborStats.Infrastructure/Data/Configurations/UsersConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using LaborStats.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaborStats.Infrastructure.Data.Configurations;
+
+public sealed class UsersConfiguration : IEntityTypeConfiguration<Users>
+{
+    public void Configure(EntityTypeBuilder<Users> builder)
+    {
+        builder.ToTable("users", "usr");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+            .HasDefaultValueSql("uuidv7()");
+
+        builder.Property(u => u.Login)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.HasIndex(u => u.Login)
+            .IsUnique();
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(255);
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder.Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.HasOne(u => u.Role)
+            .WithMany(r => r.User)
+            .HasForeignKey(u => u.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
