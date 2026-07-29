524e325b5758a8f09115985d7413a369553a3763
OskarSuchanek <os312224@student.polsl.pl>
2026-07-29 12:11:33 +0200

    Most stuff done

--- LaborStats/LaborStats.Infrastructure/DependencyInjection.cs
+++ LaborStats/LaborStats.Infrastructure/DependencyInjection.cs
@@ -38,6 +38,7 @@ public static class DependencyInjection
         services.AddScoped<IRoleService, RoleService>();
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IDataConversionService, DataConversionService>();
 
         return services.AddScoped<AdminSeeder>();
     }
