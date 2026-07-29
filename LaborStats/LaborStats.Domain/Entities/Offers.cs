1be76b2c804c19fde10b1c299d994f6ac2e82b6f
JJendrzej <Endriu7890@gmail.com>
2026-07-29 14:49:46 +0200

    Add offers entity setup

---
+++ LaborStats/LaborStats.Domain/Entities/Offers.cs
@@ -0,0 +1,14 @@
+﻿namespace LaborStats.Domain.Entities;
+
+public sealed class Offers
+{
+    public Guid Id { get; set; }
+    public int Year { get; set; }
+    public PeriodType Period { get; set; }
+    public Guid ProfessionGroupId { get; set; }
+    public ProfessionGroups ProfessionGroup { get; set; } = null!;
+    public string SnapshotCompanyName { get; set; } = null!;
+    public DateOnly? SnapshotExpirationDate { get; set; }
+    public string SnapshotJobTitle { get; set; } = null!;
+    public DateOnly SnapshotPublicationDate { get; set; }
+}
---
+++ LaborStats/LaborStats.Infrastructure/Data/Configurations/OffersConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using LaborStats.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaborStats.Infrastructure.Data.Configurations;
+
+public sealed class OffersConfiguration : IEntityTypeConfiguration<Offers>
+{
+    public void Configure(EntityTypeBuilder<Offers> builder)
+    {
+        builder.ToTable("offers","off");
+
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .HasDefaultValueSql("uuidv7()");
+
+        builder.Property(o => o.Year)
+            .IsRequired();
+
+        builder.Property(o => o.Period)
+            .HasConversion<byte>()
+            .IsRequired();
+
+        builder.Property(o => o.ProfessionGroupId)
+            .IsRequired();
+
+        builder.Property(o => o.SnapshotCompanyName)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.Property(o => o.SnapshotExpirationDate)
+            .HasColumnType("date");
+
+        builder.Property(o => o.SnapshotJobTitle)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.Property(o => o.SnapshotPublicationDate)
+            .HasColumnType("date")
+            .IsRequired();
+
+        builder.HasOne(o => o.ProfessionGroup)
+            .WithMany()
+            .HasForeignKey(o => o.ProfessionGroupId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(o => o.ProfessionGroupId);
+        builder.HasIndex(o => new { o.Year, o.Period});
+        builder.HasIndex(o => o.SnapshotPublicationDate);
+        builder.HasIndex(o => o.SnapshotExpirationDate);
+    }
+}
